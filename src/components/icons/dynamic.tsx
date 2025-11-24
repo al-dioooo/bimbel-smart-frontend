9253530a0edb2d4f545bab86419157f755eebf94
Aldio Lisafron <78518262+al-dioooo@users.noreply.github.com>
2025-11-24 14:32:01 +0700

    Fix invalid DOM property

--- src/components/icons/dynamic.tsx
+++ src/components/icons/dynamic.tsx
@@ -7,8 +7,8 @@ type Props = {
 export const ChevronUpDown = ({ className = "", strokeWidth = 1.5, direction }: Props) => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={strokeWidth} stroke="currentColor" className={className}>
-            <path d="M8 9L12 5L16 9" opacity={direction === "up" ? 1 : .5} stroke-linecap="round" stroke-linejoin="round" />
-            <path d="M16 15L12 19L8 15" opacity={direction === "down" ? 1 : .5} stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M8 9L12 5L16 9" opacity={direction === "up" ? 1 : .5} strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M16 15L12 19L8 15" opacity={direction === "down" ? 1 : .5} strokeLinecap="round" strokeLinejoin="round" />
         </svg>
     )
 }
